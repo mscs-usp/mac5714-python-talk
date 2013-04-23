0232a9b6baf414095b7411f1ff3df3f7a994aa55
Albert De La Fuente <vonpupp@gmail.com>
2013-04-23 11:13:38 -0300

    changed debug example file names

---
+++ code/04.01.00.debug.py
@@ -0,0 +1,10 @@
+# Example one
+show and edit so it breaks with pdb
+
+# Example two
+show and edit so it breaks with ipdb
+python -m ipdb 04.01.01.deck-example.py
+
+# Example three
+show and edit so it breaks with updb
+python -m updb 04.01.01.deck-example.py
